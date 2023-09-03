0ce73d58efbbf573a07ac362ee99c7c3a8fb14ae
Yuki <50564056+yuki-konn@users.noreply.github.com>
2023-09-02 20:31:41 -0500

    Add files via upload
    Initial commit

---
+++ askii_phone.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /*
+    Escape sequences | Askii Phone
+    \n for newline
+    \t for tab
+    */
+    // printf("------------------- \n");
+    printf("|\t1\t|\t2\t|\t3\t|\n");
+    printf("|\t\t|\tABC\t|\tDEF\t|\n");
+    // printf("|-------|-------|-------| \n");
+    printf("|\t4\t|\t5\t|\t6\t|\n");
+    printf("|\tGHI\t|\tJKL\t|\tMNO\t|\n");
+    // printf("|-------|-------|-------| \n");
+    printf("|\t7\t|\t8\t|\t9\t|\n");
+    printf("|\tPQRS\t|\tTUV\t|\tWXYZ\t|\n");
+    // printf("|-------|-------|-------| \n");
+    printf("|\t*\t|\t0\t|\t#\t|\n");
+    printf("|\t\t|\t+\t|\t\t|\n");
+    printf("|\t\t\t\t\t\t|\n");
+    printf("|\t\t\tCall\t\t\t|\n");
+
+    /* printf("-------------------------\n");
+    printf("|         Call          |\n");
+    printf("-------------------------\n"); */
+    return 0;
+}
